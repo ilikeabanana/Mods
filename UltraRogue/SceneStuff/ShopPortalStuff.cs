6a39100306b81f45f0c787de8f3f97328ed9c563
Michiel De Backer <michiel1.debacker@gmail.com>
2026-05-19 17:47:07 +0200

    Shop room fix and unfinished new crusher room.

--- UltraRogue/SceneStuff/ShopPortalStuff.cs
+++ UltraRogue/SceneStuff/ShopPortalStuff.cs
@@ -9,24 +9,57 @@ public class ShopPortalStuff : MonoBehaviour
 {
     [SerializeField] Door DoorToRunSimpleOpenOverrideOn;
     [SerializeField] string PortalExitFinder = "ShopPortalExit";
+    [SerializeField] string PortalExitSlot = "ShopHolders";
     [Tooltip("If null, will just do GetComponent to get it")]
     [SerializeField] Portal PortalScript;
-
+    GameObject Exit;
+    private Transform slot;
+    GameObject[] ForeignExit = { null, null };
     bool isOpen = false;
 
     void Awake()
     {
+
         isOpen = false;
         if (PortalScript == null) PortalScript = GetComponent<Portal>();
 
 
         if (GameObject.Find(PortalExitFinder))
         {
-            PortalScript.exit = GameObject.Find(PortalExitFinder).transform;
+            Exit = Instantiate(GameObject.Find(PortalExitFinder));
+            slot = GameObject.Find(PortalExitSlot).transform;
+            Exit.transform.parent = slot;
+            Exit.transform.localPosition = Vector3.zero;
+            PortalScript.exit = Exit.transform;
         }
 
     }
 
+    public void Fake()
+    {
+        Exit.transform.localPosition = Vector3.zero;
+        if (ForeignExit[0] == null)
+        {
+            int i = 0;
+            foreach (Transform child in slot)
+            {
+                {
+                    if (child != Exit.transform)
+                    {
+                        ForeignExit[i++] = child.gameObject;
+                    }
+                }
+            }
+        }
+
+        foreach (GameObject go in ForeignExit)
+        {
+
+            go.transform.localPosition = new Vector3(0, -50, 0);
+
+        }
+
+    }
     void Open()
     {
         if (!DoorToRunSimpleOpenOverrideOn.open)
