bd906ce177f33878881a661f8ebb58215602e0ec
MichielBanana <m.debacker@miniemen.school>
2026-06-12 22:30:31 +0200

    wowie i hate not having unity

--- UltraRogue/Behaviours/Missle.cs
+++ UltraRogue/Behaviours/Missle.cs
@@ -60,7 +60,7 @@ public class Missle : MonoBehaviour
         Vector3 dir = (point - transform.position).normalized;
 
         if (rb.velocity.sqrMagnitude > 0.01f)
-            transform.rotation = Quaternion.LookRotation(rb.velocity.normalized) * Quaternion.Euler(-90f, 0f, 0f);
+            transform.rotation = Quaternion.LookRotation(rb.velocity.normalized) * Quaternion.Euler(90f, 0f, 0f);
 
         Vector3 newVelocity = Vector3.Lerp(rb.velocity, dir * speed, turnSpeed * Time.fixedDeltaTime);
         rb.velocity = newVelocity;
